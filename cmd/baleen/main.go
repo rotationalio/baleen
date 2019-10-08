c23ae3346a5184bda493e986f310dacdc6751880
Rebecca Bilbro <rebeccabilbro@users.noreply.github.com>
2019-10-08 13:47:54 -0400

    Add initial feeds for spanish, korean, greek, and multilingual (#3)
    Adds initial feeds in opml and json form and implements reading from opml.

--- cmd/baleen/main.go
+++ cmd/baleen/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
+	"io/ioutil"
 	"os"
+
+	"path/filepath"
 
 	"github.com/kansaslabs/baleen"
 	"github.com/kansaslabs/baleen/fetch"
+	"github.com/kansaslabs/baleen/utils"
 	"gopkg.in/urfave/cli.v1"
 )
 
@@ -31,35 +36,71 @@ func main() {
 }
 
 func run(c *cli.Context) (err error) {
-	if c.NArg() == 0 {
-		return cli.NewExitError("specify a feed to fetch", 1)
-	}
+	var root = filepath.Join("fixtures")
+	var files []string
+	var urls []string
 
-	url := c.Args()[0]
-	fetcher := fetch.New(url)
-	feed, err := fetcher.Fetch()
-
-	if err != nil {
-		switch he := err.(type) {
-		case fetch.HTTPError:
-			if he.NotFound() {
-				fmt.Println("the url you supplied was not valid")
+	// If the user specifies a feed via the command line, only get that one
+	if c.NArg() > 0 {
+		urls = append(urls, c.Args()[0])
+	} else {
+		// Otherwise retrieve feeds from files in the fixtures directory
+		err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+			files = append(files, path)
+			return nil
+		})
+		if err != nil {
+			panic(err)
+		}
+		for _, file := range files {
+			switch filepath.Ext(file) {
+			case ".opml":
+				o := utils.OPML{}
+				content, _ := ioutil.ReadFile(file)
+				err := xml.Unmarshal(content, &o)
+				if err != nil {
+					panic(err)
+				}
+				for _, outline := range o.Body.Outlines {
+					urls = append(urls, outline.XMLURL)
+				}
+			case ".json":
+				fmt.Println("parsing from json not yet implemented")
 			}
-			if he.NotModified() {
-				fmt.Println("no new items in the feed!")
-			}
-			return cli.NewExitError(err, 1)
-		default:
-			return cli.NewExitError(err, 1)
 		}
 	}
 
-	fmt.Println(feed.Title)
-	fmt.Println(feed.Description + "\n")
+	// Return an error is no feed was specified in the cli and none were retrieved from fixtures
+	if len(urls) == 0 {
+		return cli.NewExitError("specify a feed to fetch or add feeds to fixtures directory", 1)
+	}
 
-	for _, item := range feed.Items {
-		fmt.Println(item.Title)
-		fmt.Println(item.Description + "\n")
+	for _, url := range urls {
+		fetcher := fetch.New(url)
+		feed, err := fetcher.Fetch()
+
+		if err != nil {
+			switch he := err.(type) {
+			case fetch.HTTPError:
+				if he.NotFound() {
+					fmt.Println("the url you supplied was not valid")
+				}
+				if he.NotModified() {
+					fmt.Println("no new items in the feed!")
+				}
+				return cli.NewExitError(err, 1)
+			default:
+				return cli.NewExitError(err, 1)
+			}
+		}
+
+		fmt.Println(feed.Title)
+		fmt.Println(feed.Description + "\n")
+
+		for _, item := range feed.Items {
+			fmt.Println(item.Title)
+			fmt.Println(item.Description + "\n")
+		}
 	}
 
 	return nil
---
+++ utils/opml.go
@@ -0,0 +1,35 @@
+package utils
+
+import (
+	"encoding/xml"
+)
+
+// An OPML struct can be loaded from an OPML file
+type OPML struct {
+	XMLName xml.Name `xml:"opml"`
+	Version string   `xml:"version,attr"`
+	Head    Head     `xml:"head"`
+	Body    Body     `xml:"body"`
+}
+
+// The Head of the opml
+type Head struct {
+	XMLName xml.Name `xml:"head"`
+	Title   string   `xml:"title"`
+}
+
+// The Body of the opml
+type Body struct {
+	XMLName  xml.Name  `xml:"body"`
+	Outlines []Outline `xml:"outline"`
+}
+
+// The Outline of the opml
+type Outline struct {
+	Text    string `xml:"text,attr"`
+	Title   string `xml:"title,attr"`
+	Type    string `xml:"type,attr"`
+	XMLURL  string `xml:"xmlUrl,attr"`
+	HTMLURL string `xml:"htmlUrl,attr"`
+	Favicon string `xml:"rssfr-favicon,attr"`
+}
