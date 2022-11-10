053051bdde3bf43cd2fcd1f0e1f0f9007d5a7cd1
Benjamin Bengfort <benjamin@bengfort.com>
2022-11-09 23:20:11 -0500

    version bump to v0.2.0

--- version.go
+++ version.go
@@ -5,10 +5,10 @@ import "fmt"
 // Version component constants for the current build.
 const (
 	VersionMajor         = 0
-	VersionMinor         = 1
+	VersionMinor         = 2
 	VersionPatch         = 0
-	VersionReleaseLevel  = ""
-	VersionReleaseNumber = 1
+	VersionReleaseLevel  = "beta"
+	VersionReleaseNumber = 2
 )
 
 // Set the GitVersion via -ldflags="-X 'github.com/rotationalio/baleen/pkg.GitVersion=$(git rev-parse --short HEAD)'"
